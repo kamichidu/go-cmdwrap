dc5a22c8c08048e1f337ace3b31a3ffc2cb7ce7c
kamichidu <c.kamunagi@gmail.com>
2017-09-13 16:07:47 +0900

    Add -r and -l flag

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/joho/godotenv"
 )
 
 type envVars []string
@@ -24,9 +26,13 @@ func (self *envVars) String() string {
 func run(in io.Reader, out io.Writer, errOut io.Writer, args []string) int {
 	flags := flag.NewFlagSet(filepath.Base(args[0]), flag.ExitOnError)
 	var (
-		env = &envVars{}
+		env       = &envVars{}
+		useDotenv bool
+		listEnvs  bool
 	)
 	flags.Var(env, "e", "Environment variables")
+	flags.BoolVar(&useDotenv, "r", false, "Read enviroment variables from .env")
+	flags.BoolVar(&listEnvs, "l", false, "List environment variables")
 	if err := flags.Parse(args[1:]); err != nil {
 		log.Printf("Argument error: %s", err)
 		return 128
@@ -34,6 +40,21 @@ func run(in io.Reader, out io.Writer, errOut io.Writer, args []string) int {
 		log.Printf("No positional arguments")
 		return 128
 	}
+
+	if useDotenv {
+		if err := godotenv.Load(); err != nil {
+			log.Printf("Unable to load .env file: %s", err)
+			return 1
+		}
+	}
+
+	if listEnvs {
+		for _, envVar := range os.Environ() {
+			fmt.Fprintln(out, envVar)
+		}
+		return 0
+	}
+
 	cmdName := flags.Args()[0]
 	var cmdArgs []string
 	if flags.NArg() > 1 {
